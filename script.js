c8e0e34c689663960e810508dbb652ecca1195ea
Ryan Brown <rkbrown25@meta.com>
2023-06-15 15:06:11 -0700

    Minor CSS grid change and populating home screen with films

--- script.js
+++ script.js
@@ -1,36 +1,65 @@
-var searchBtn = document.getElementById("submitbtn")
-var movieContainer = document.getElementById("moviecontainer")
+var searchBtn = document.getElementById("submitbtn");
+var movieContainer = document.getElementById("moviecontainer");
+var loadBtn = document.getElementById("loadBtn");
 
+document.addEventListener("DOMContentLoaded", async (event) => {
+  event.preventDefault();
 
+  try {
+    const apiKey = "0d02fe98abd1fa29b643a231a9ac3b49";
 
-searchBtn.addEventListener("click", async (event) => {
-    event.preventDefault()
+    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
 
-    try {
-        //const apiKey = "Hcncqhk28zhby6Khi8w9tsyLpYKYKYun"
-        //const searchTerm = "hello"
-        //const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(searchForm.value)}`
-        const apiKey = "0d02fe98abd1fa29b643a231a9ac3b49"
+    const response = await fetch(url);
+    const data = await response.json();
 
-        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
+    const results = data.results;
 
+    // POPULATING LANDING SCREEN WITH 6 FILMS
+    for (let i = 0; i < 6; i++) {
+      ///////////////
+      // For each film, access film's info (movie), create the appropriate HTML structure with classnames (see comment in index.HTML for structure), and fill with
+      // required data
+      ///////////////
+      const movie = results[i];
 
-        const response = await fetch(url)
-        const data = await response.json()
-        
-        const results = data.results
+      const movieItemContainer = document.createElement("div");
+      movieItemContainer.className = "moviecontainer--movie";
 
-        const sample = results[0].poster_path;
+      const moviePoster = document.createElement("img");
+      moviePoster.className = "moviecontainer--movie_img";
+      moviePoster.src = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
 
-        const img = document.createElement("img")
-        img.src = `https://image.tmdb.org/t/p/original/${sample}`
-        movieContainer.appendChild(img)
+      const movieInfoContainer = document.createElement("div");
+      movieInfoContainer.className = "moviecontainer--movie_info";
 
+      const movieRatingContainer = document.createElement("div");
+      movieRatingContainer.className = "moviecontainer--movie_info-rating";
 
+      const starImg = document.createElement("img");
+      starImg.className = "moviecontainer--movie_info-rating_star";
+      starImg.src = "25533-transformed.png";
 
-        console.log(data)
-    } catch (error) {
-        console.log(error)
+      const movieRatingValue = document.createElement("p");
+      movieRatingValue.innerText = `${movie.vote_average}`;
+
+      movieRatingContainer.appendChild(starImg);
+      movieRatingContainer.appendChild(movieRatingValue);
+
+      const movieTitle = document.createElement("p");
+      movieTitle.innerText = `${movie.title}`;
+
+      movieInfoContainer.appendChild(movieRatingContainer);
+      movieInfoContainer.appendChild(movieTitle);
+
+      movieItemContainer.appendChild(moviePoster);
+      movieItemContainer.appendChild(movieInfoContainer);
+
+      movieContainer.appendChild(movieItemContainer);
     }
-    console.log("Button clicked")
-})
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
